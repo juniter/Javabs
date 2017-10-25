decbb539ba417344c5a705402c6bcaf19666f147
Juniter <juniterr@outlook.com>
2017-10-25 15:00:02 +0800

    add log4j2 yaml config support

--- src/main/java/com/juniter/javabs/Application.java
+++ src/main/java/com/juniter/javabs/Application.java
@@ -2,7 +2,6 @@ package com.juniter.javabs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,15 +16,18 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(DefaultConfiguration.class)
 public class Application implements CommandLineRunner{
-	private static final Logger logger = LoggerFactory.getLogger(Application.class);
-	
+	private static final Logger logger =LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 	
 	@Override
 	public void run(String... arg0) throws Exception {
-		System.out.println("Hello World!");
-		logger.info("输出信息：{},Email:{}","Hello World");		
+		for (int i =0;i<2;i++) {
+			logger.info("输出信息：{},Email:{}","Hello World");		
+			logger.debug("输出信息：{},Email:{}","Hello World");
+			logger.warn("输出信息：{},Email:{}","Hello World");
+			logger.error("输出信息：{},Email:{}","Hello World");
+		}
 	}
 }
